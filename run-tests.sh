4e0888900c540b184afc4afaeb97dec9f1b0ae14
sasha <xrombik@yandex.ru>
2020-07-11 12:37:54 +0300

    Доработка СНИ

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 cd ./build
-
-./strs-test || echo "ошибка strs"
-./inifile-test "../config0.ini" "../config1.ini" "../config2.ini" || echo "ошибка inifile"
+set -e
+echo "проверка strs" || echo "    *** ошибка ***"
+./strs-test
+echo "проверка 1 inifile"
+./inifile-test "../config0.ini" "../config1.ini" "../config2.ini" || echo "    *** ошибка ***"
+echo "проверка 2 inifile"
+cmp -s "../config0.ini" "../config1.ini" || echo "    *** ошибка ***"
